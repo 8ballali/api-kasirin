5d6016b13b58bfa67d13a1093dc428fd891746c9
8ballali <iqbalalisaidil@outlook.com>
2021-11-26 13:32:45 +0700

    add filter transaction

--- app/Http/Controllers/api/FilterStrukController.php
+++ app/Http/Controllers/api/FilterStrukController.php
@@ -11,8 +11,21 @@ class FilterStrukController extends Controller
 {
     public function index(Request $request)
     {
+
         $start = Carbon::parse($request->tanggal_mulai);
         $end = Carbon::parse($request->tanggal_selesai);
+        if (!$request->tanggal_mulai) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please Input Your Date'
+            ],400);
+        }
+        if (!$request->tanggal_selesai) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please Input Your Date'
+            ],400);
+        }
         $filter = Transaction::when(($request->get('store_id')), function ($query) use ($request)
         {
             $query->where('store_id', $request->store_id);
