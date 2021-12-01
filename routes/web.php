5c41fb60e9e6deefaea3fe0e35c3c8524cab928f
8ballali <iqbalalisaidil@outlook.com>
2021-12-01 09:41:14 +0700

    Trend Transaksi

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::post('/post_login', [AuthController::class, 'postLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/login', function () {
-    return view('welcome');
-});
+
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
